f670d286acec3ce9db2cf278bd2c51cc124e3264
David Colon <dcolon@shoprunner.com>
2015-08-06 16:56:15 -0400

    chroot

--- create-account.sh
+++ create-account.sh
@@ -8,12 +8,12 @@ if [ $# -ne 1 ]; then
 fi
 
 uid=$1
-partner=(account1 account2 account3)
-last=${#partner[@]}
+newuser=(account1 account2 account3)
+last=${#newuser[@]}
 
 echo "starting at uid $uid"
 echo "ending at uid $last"
-echo "list of partners to add: ${partner[@]}"
+echo "list of newusers to add: ${newuser[@]}"
 echo -n "Is this correct [yes or no]? "
 read answer
 if [ "$answer" != "yes" ]; then
@@ -23,10 +23,10 @@ fi
 
 for ((i=0; i<$last; i++))
 do
-    useradd -u $((i+uid)) -G chrooted -c "${partner[$i]}" ${partner[$i]}
-    mkdir -p /home/${partner[$i]}
-    chown ${partner[$i]}.${partner[$i]} /home/${partner[$i]}
+    useradd -u $((i+uid)) -G chrooted -c "${newuser[$i]}" ${newuser[$i]}
+    mkdir -p /chroot/${newuser[$i]}
+    chown ${newuser[$i]}.${newuser[$i]} /chroot/${newuser[$i]}
     pass=$(cat /dev/urandom|tr -dc "a-zA-Z0-9-_\$\?&*@#%^&()+!=|,." | fold -w 9 | head -1)
-    echo "${partner[$i]}:${pass}" | chpasswd
-    echo "${partner[$i]}:${pass}"
+    echo "${newuser[$i]}:${pass}" | chpasswd
+    echo "${newuser[$i]}:${pass}"
 done
